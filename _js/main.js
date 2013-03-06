83203d1e5b30009697a1f6fe04edfdda45d8ac57
Daniel Beacham <dkuwf15@gmail.com>
2013-03-05 23:35:01 -0500

    Updated to CRUD layout

--- _js/main.js
+++ _js/main.js
@@ -1,1 +1,20 @@
 // JS for ASD
+$(document).on('pageinit', function(){
+	bindFormElements();
+});
+
+var bindFormElements = function() {
+	var $mapContainer = $('#map_container');
+
+	$mapContainer.hide();
+
+	$('#memory_geotag').on('change', function(e) {
+		var val = $(this).val();
+		if (val == 1 && $mapContainer.is(':hidden')) {
+			$mapContainer.show();
+		} else {
+			$mapContainer.hide();
+		}
+	});
+};
+
